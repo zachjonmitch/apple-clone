f0eb317447086072f648d8e6aa32e35e7397dfd9
Zach Mitchell <zjmitche@gmail.com>
2017-04-23 09:50:12 -0600

    Large display images and mobile viewport

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
     var currentSlide = 1;
 
     var $sliderWrap = $('#sliderWrap');
+    var $navOne = $('.navOne');
+    var $navTwo = $('.navTwo');
+    var $navThree = $('.navThree');
 
-    setInterval(function(){
+    //Slider timer animation
+    var slideTimer = setInterval(function(){
         $sliderWrap.animate({'margin-left': '-=100%'}, animationSpeed, function() {
             currentSlide++
             if (currentSlide === 4) {
@@ -14,4 +18,32 @@ $(document).ready(function() {
             }
         });
     }, delay)
-})
+
+    //Slider navigation events
+    $navOne.click(function() {
+        $(this).css('background', 'black');
+        $navTwo.css('background', 'rgba(128,128,128,0.8)');
+        $navThree.css('background', 'rgba(128,128,128,0.8)');
+        
+        $sliderWrap.css('margin-left', 0);
+        clearInterval(slideTimer);
+    });
+
+    $navTwo.click(function() {
+        $(this).css('background', 'black');
+        $navOne.css('background', 'rgba(128,128,128,0.8)');
+        $navThree.css('background', 'rgba(128,128,128,0.8)');
+
+        $sliderWrap.css('margin-left', '-100%');
+        clearInterval(slideTimer);
+    });
+
+    $navThree.click(function() {
+        $(this).css('background', 'black');
+        $navOne.css('background', 'rgba(128,128,128,0.8)');
+        $navTwo.css('background', 'rgba(128,128,128,0.8)');
+
+        $sliderWrap.css('margin-left', '-200%');
+        clearInterval(slideTimer);
+    });
+});
